f6c033fc83f2bc34c711f5f27f3d91c491e6aaf2
Александр Попов <git@apopov.app>
2026-01-10 00:27:40 +0300

    feat: автоматическое разрешение конфликтов имен пакетов + флаг --name
    - Добавлена функция findModuleRoot для поиска корня Go модуля
    - Добавлена функция pathToUniqueName для преобразования пути в уникальное имя
    - Автоматическое разрешение конфликтов: одинаковые имена пакетов в разных местах получают уникальные имена на основе пути
    - Добавлен флаг --name для ручного задания имени пакета
    - Исправлена функция title в шаблонах для правильной обработки подчеркиваний
    - Обновлен README с описанием новой функциональности
    - Добавлен example3 демонстрирующий автоматическое разрешение конфликтов

---
+++ example3/cmd/Bbin/internal/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Server represents an HTTP server
+type Server struct {
+	httpServer *http.Server
+	addr       string
+}
+
+// NewFromConfig creates a new HTTP server using the provided config.
+// Defaults are defined here (close to where they're used), not in main.
+func NewFromConfig(config Config) (*Server, string, error) {
+	if config == nil {
+		return nil, "", fmt.Errorf("nil config")
+	}
+
+	// Defaults live here, in the package that uses them
+	host, _ := config.Host("0.0.0.0")
+	port, _ := config.Port(8081)
+
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	srv := &Server{
+		httpServer: &http.Server{
+			Addr:    addr,
+			Handler: http.NewServeMux(),
+		},
+		addr: addr,
+	}
+
+	log.Printf("[Bbin] Server configured: %s", addr)
+
+	return srv, addr, nil
+}
+
+// Start starts the HTTP server
+func (s *Server) Start() error {
+	log.Printf("[Bbin] Starting server on %s", s.addr)
+	return s.httpServer.ListenAndServe()
+}
+
+// GetAddr returns the server address
+func (s *Server) GetAddr() string {
+	return s.addr
+}
---
+++ example3/go.mod
@@ -0,0 +1,9 @@
+module github.com/apopov-app/ggconfig/example3
+
+go 1.21
+
+require github.com/apopov-app/ggconfig v0.0.0
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
+
+replace github.com/apopov-app/ggconfig => ../
